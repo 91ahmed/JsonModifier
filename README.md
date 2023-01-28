2359bd3a7750edb50175ddede49c7a0c12b2ba1a
91ahmed <ahmedh12491@gmail.com>
2023-01-28 03:58:08 +0200

    new update

--- README.md
+++ README.md
@@ -21,9 +21,15 @@ JsonModifier was created to help php developers modify Json files by performing 
 ]
 ```
 
-#### Install via composer
+#### Install
+via composer
+```
+The composer package will be ready as soon as possible.
 ```
 
+clone git repository or download the zip file.
+```
+git clone https://github.com/91ahmed/JsonModifier.git
 ```
 
 #### How to Use
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '41a881806928c948d1b7e094b0166a13913fdb9e',
+        'name' => 'phpjsonmodifier/pjson-modifier',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpjsonmodifier/pjson-modifier' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '41a881806928c948d1b7e094b0166a13913fdb9e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
